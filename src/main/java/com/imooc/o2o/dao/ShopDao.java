7ef8122807d72c743cd3809ccc25cd079ad68862
Yifan <yifanluo33@gmail.com>
2020-08-26 14:20:55 +0800

    店铺列表展示-Dao 层实现，8.26

--- src/main/java/com/imooc/o2o/dao/ShopDao.java
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -1,8 +1,29 @@
 package com.imooc.o2o.dao;
 
 import com.imooc.o2o.entity.Shop;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShopDao {
+
+    /**
+     * 分页查询店铺，可输入的条件有：店铺名（模糊），店铺状态，店铺类别，店铺 ID，owner id
+     * @param shopCondition 查询条件
+     * @param rowIndex 从第几行开始取数据
+     * @param pageSize 返回的条数
+     * @return
+     */
+    List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex,
+                             @Param("pageSize") int pageSize);
+
+    /**
+     * 返回queryShopList 总数
+     * @param shopCondition
+     * @return
+     */
+    int queryShopCount(@Param("shopCondition") Shop shopCondition);
+
     /**
      * 通过 shop id 查询店铺
      * @param shopId
