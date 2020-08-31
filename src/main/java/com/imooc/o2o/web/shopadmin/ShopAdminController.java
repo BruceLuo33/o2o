c4c463c70a4091812adbef669e6604b4cc4f9df5
Yifan <yifanluo33@gmail.com>
2020-08-31 17:46:45 +0800

    商品添加功能进行中，前端实现debug 中。8.31

--- src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ShopAdminController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "shopadmin", method = {RequestMethod.GET})
+/**
+ * 主要用来解析路由并转发到相应的 html 中
+ */
 public class ShopAdminController {
     @RequestMapping(value = "/shopoperation")
     public String shopOperation() {
@@ -24,5 +27,11 @@ public class ShopAdminController {
     @RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
     private String productCategoryManage() {
         return "shop/productcategorymanagement";
+    }
+
+    @RequestMapping(value = "productoperation")
+    public String productOperation() {
+        // 转发至商品添加 or 编辑页面
+        return "shop/productoperation";
     }
 }
