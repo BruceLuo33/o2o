c3fab6afd911c4443eff31b6e3def60a495330fc
BruceLuo33 <yifanluo33@gmail.com>
2020-09-16 15:12:38 +0800

    商店列表页前端设计开发完成。 9.16

--- src/main/java/com/imooc/o2o/util/PathUtil.java
+++ src/main/java/com/imooc/o2o/util/PathUtil.java
@@ -13,7 +13,7 @@ public class PathUtil {
         String os = System.getProperty("os.name");
         String basePath = "";
         if (os.toLowerCase().startsWith("win")) {
-            basePath = "D:/JavaProject/image/";
+            basePath = "D:/JavaProject/image";
         } else {
             basePath = "/home/yifan/image/";
         }
@@ -22,7 +22,7 @@ public class PathUtil {
     }
 
     public static String  getShopImagePath(long shopId) {
-        String imagePath = "upload/item/shop/" +shopId + "/";
+        String imagePath = "/upload/item/shop/" +shopId + "/";
         return imagePath.replace("/", separator);
     }
 }
